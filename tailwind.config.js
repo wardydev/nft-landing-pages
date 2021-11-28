a4c3ffd797b16da096cf7a364175a2154cae6b31
wardy dev <wardydev@gmail.com>
2021-11-29 00:19:56 +0800

    hero section

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: "#DF45EC",
+          dark: "#3257DA"
+        },
+        secondary: {
+          light: "#FFB443",
+          dark: "#B213DA"
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
